987ed102e3731824477c4385a8c9842248977437
Carlos Benavides <1992cab@gmail.com>
2019-08-15 00:23:11 -0500

    chore: added charts library

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,23 +1,38 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
-import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { ChartsModule } from 'ng2-charts';
+
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+import { BoosterComponent } from './components/booster/booster.component';
+import { DonutGraphicComponent } from './components/donut-graphic/donut-graphic.component';
 
 @NgModule({
     declarations: [
         BreadcrumbsComponent,
         HeaderComponent,
-        SidebarComponent
+        SidebarComponent,
+        BoosterComponent,
+        DonutGraphicComponent
     ],
     imports: [
         CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        ChartsModule
     ],
     exports: [
         CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
         BreadcrumbsComponent,
         HeaderComponent,
-        SidebarComponent
+        SidebarComponent,
+        BoosterComponent,
+        ChartsModule,
+        DonutGraphicComponent
     ],
     providers: [],
 })
