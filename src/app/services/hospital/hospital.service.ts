72c383033a40c24f1e44f819527d723338f2d609
Carlos Benavides <1992cab@gmail.com>
2019-09-06 16:53:54 -0500

    chore: added hospital services

---
+++ src/app/services/hospital/hospital.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { IHospital } from 'src/app/models/hospital.model';
+import { HttpClient } from '@angular/common/http';
+import { API_URL } from 'src/app/app.config';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import Swal from 'sweetalert2';
+import { UserService } from '../user/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HospitalService {
+
+  urlResource = `${API_URL}/hospital`;
+
+  constructor(public http: HttpClient, public userService: UserService) { }
+
+  create(hospital: IHospital): Observable<any> {
+    const url = `${this.urlResource}?token=${this.userService.token}`;
+    return this.http.post(url, hospital).pipe(map((res: any) => {
+      Swal.fire(res.message, res.data.email, 'success');
+      return res;
+    }));
+  }
+
+  update(hospital: IHospital) {
+    const url = `${this.urlResource}/${hospital._id}?token=${this.userService.token}`;
+    return this.http.put(url, hospital).pipe(map((res: any) => {
+      Swal.fire(res.message, 'Hospital has been updated successfully', 'success');
+      return res;
+    }));
+  }
+
+  getAll(from = 0) {
+    const url = `${this.urlResource}?from=${from}`;
+    return this.http.get(url).pipe(map((res: any) => res));
+  }
+
+  getById(id: string) {
+    const url = `${this.urlResource}/${id}`;
+    return this.http.get(url).pipe(map((res: any) => res));
+  }
+
+  searchAll(text: string) {
+    const url = `${API_URL}/search/collection/hospitals/${text}`;
+    return this.http.get(url).pipe(map((res: any) => res));
+  }
+
+  delete(id: string): Observable<any> {
+    const url = `${this.urlResource}/${id}?token=${this.userService.token}`;
+    return this.http.delete(url).pipe(map((res: any) => res));
+  }
+}
